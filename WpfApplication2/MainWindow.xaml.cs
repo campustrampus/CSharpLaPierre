ef9cbaab232bfc93c0cf373bbb70e060f949bbea
campustrampus <lapierreni@gmail.com>
2015-05-07 10:31:35 -0400

    Establish Database

--- WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplication2
+namespace CSharpLaPierre
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
